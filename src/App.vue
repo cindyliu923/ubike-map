e7d22476e55df62a7429d0e5606c10e4da90aa2a
Cindy Liu <cindyliu923@gmail.com>
2020-05-23 17:23:24 +0800

    Add ubike map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row no-gutters">
+      <!-- 選擇地區 -->
+      <div class="toolbox col-sm-3 p-2 bg-white">
+        <div class="form-group d-flex">
+          <label for="city" class="col-form-label mr-2 text-right">縣市</label>
+          <div class="flex-fill">
+            <select id="city" class="form-control" v-model="select.city">
+              <!-- 製作下拉選單 -->
+              <option v-bind:value="city.name" v-bind:key="city.name" v-for="city in cityName">
+                {{ city.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group d-flex">
+          <label for="dist" class="col-form-label mr-2 text-right">地區</label>
+          <div class="flex-fill">
+            <select id="dist" class="form-control" v-model="select.dist">
+              <!-- 製作下拉選單 -->
+              <option :value="dist.name" :key="dist.name"
+                v-for="dist in cityName.find((city) => city.name == select.city).districts">
+                {{ dist.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- 顯示地圖和 UBike 站點 -->
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import L from 'leaflet';
+import cityName from './assets/cityName.json';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data: () => ({
+    cityName,
+    select: {
+      city: '台北市',
+      dist: '中正區',
+    },
+    ubikes: [],
+    OSMap: {},
+  }),
+  computed: {
+    youbikes() {
+      return this.ubikes.filter((bike) => bike.sarea === this.select.dist);
+    },
+  },
+  watch: {
+    youbikes() {
+      this.updateMap();
+    },
+  },
+  methods: {
+    updateMap() {
+      this.OSMap.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+      });
+
+      this.youbikes.forEach((bike) => {
+        L.marker([bike.lat, bike.lng]).bindPopup(`
+          <p>
+          <strong style="font-size: 20px;">${bike.sna}</strong></p>
+          <strong style="font-size: 16px; color: #d45345;">可租借車輛剩餘：${bike.sbi} 台</strong>
+          <br>
+            可停空位剩餘: ${bike.bemp}
+          <br>
+          <small>最後更新時間: ${bike.mday}</small>
+        `).addTo(this.OSMap);
+      });
+
+      this.cityName[0].districts.find((dist) => {
+        if (dist.name === this.select.dist) {
+          // this.OSMap.panTo(new L.LatLng(dist.latitude, dist.longitude));
+          this.OSMap.flyTo(new L.LatLng(dist.latitude, dist.longitude), 14);
+        }
+        return dist.name === this.select.dist;
+      });
+    },
+  },
+  created() {
+    const api = 'https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json';
+    this.$http.get(api).then((response) => {
+      this.ubikes = Object.values(response.data.retVal);
+    });
+  },
+  // 畫面結束後用的 hook
+  mounted() {
+    this.OSMap = L.map('map', {
+      center: [25.041956, 121.508791],
+      zoom: 18,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+    }).addTo(this.OSMap);
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import 'bootstrap/scss/bootstrap';
+
+#map {
+  height: 100vh;
+  position: relative;
 }
 </style>
